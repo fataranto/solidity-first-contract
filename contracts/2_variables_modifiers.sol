8804a33cbf65b2f74e9dfd9bbeb9ce086c07b3f5
fataranto <fataranto@gmail.com>
2024-09-19 23:05:40 +0200

    variables (en proceso)

---
+++ contracts/2_variables_modifiers.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+// Version
+pragma solidity ^0.8.4;
+
+contract variables_modifiers {
+
+    //valores enteros sin signos (uint)
+    uint256 a;
+    uint8 public b = 3;
+
+}

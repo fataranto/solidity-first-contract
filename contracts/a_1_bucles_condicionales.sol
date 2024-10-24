db0648bfc9bd0ff4905bb5deb31e2a9ac431c5e9
fataranto <fataranto@gmail.com>
2024-10-24 19:14:06 +0200

    bucles y condicionales para operaciones matematicas basicas

---
+++ contracts/a_1_bucles_condicionales.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+// Version
+pragma solidity ^0.8.4;
+
+contract loops_conditionals {
+
+    //suma de los 10 primeros numeros a partir de un numero introducido
+    function sum(uint _number) public pure returns (uint){
+        uint aux_sum = 0;
+
+        // Bucle for
+        for (uint i = _number; i < (10 + _number); i++){
+            aux_sum = aux_sum + i;
+        }
+
+        return aux_sum;
+    }
+
+
+    // Suma de los 10 primeros numeros impares
+    function odd() public pure returns (uint){
+        uint aux_sum = 0;
+        uint counter = 0;
+        uint counter_odd = 0;
+
+        while(counter_odd < 10){
+
+            if (counter % 2 != 0){
+                aux_sum = aux_sum + counter;
+                counter_odd++;
+            }
+
+            counter++;
+        }
+
+        return aux_sum;
+    }
+
+
+    function operations(string memory operation, uint a, uint b) public pure returns (uint){
+
+        bytes32 hash_operation = keccak256(abi.encodePacked(operation));
+        if (hash_operation == keccak256(abi.encodePacked("suma"))) {
+            return a+b;
+        } 
+      
+        else if (hash_operation == keccak256(abi.encodePacked("resta"))) {
+            return a-b;
+        }
+
+        else if (hash_operation == keccak256(abi.encodePacked("division"))) {
+            return a/b;
+        }
+
+        else if (hash_operation == keccak256(abi.encodePacked("multiplicacion"))) {
+            return a*b;
+        }
+
+        else {
+            return 0;
+        }
+
+    }
+}

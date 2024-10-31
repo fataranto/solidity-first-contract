dc8a42c0e921a69f68145057f2e1d603ec3b9416
fataranto <fataranto@gmail.com>
2024-10-31 19:40:50 +0100

    ERC-20 funciones elementales

--- contracts/b_2_ERC-20-propio.sol
+++ contracts/b_2_ERC-20-propio.sol
@@ -27,6 +27,70 @@ interface IERC20 {
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+abstract contract ERC20 is IERC20 {
+    mapping(address => uint256) private _balances;
+    //Owner (Fabi) -> Spender (Alberto) -> 5 tokens
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name_, string memory symbol_){
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() public view virtual returns (string memory){
+        return _name;
+    }
+
+    function symbol() public view virtual returns (string memory){
+        return _symbol;
+    }
+
+    function decimals() public view virtual returns (uint8){
+        return 18;
+    }
+
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer() public virtual override returns (bool){
+        address owner = msg.sender;
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public virtual override returns (uint256){
+        _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256) public virtual override returns (bool){
+        address owner = msg.sender;
+        _approve(owner, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount) public virtual override returns (bool){
+        address spender = msg.sender;
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+    
+
 
 
 }
